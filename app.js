f90bcae5c72394a9dee34c27d36b6333d9222ae7
= <=>
2022-08-20 16:55:24 +0530

    Done

---
+++ app.js
@@ -0,0 +1,20 @@
+var btnTranslate=document.querySelector('#btn-translate');
+var inputtextarea=document.querySelector('#textarea-input');
+var outputtextarea=document.querySelector('#textarea-output');
+
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json";
+
+function getTranslationUrl(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+btnTranslate.addEventListener("click",function clickEventListner() {
+    var input = inputtextarea.value;
+
+    fetch(getTranslationUrl(input))
+    .then(response => response.json())
+    .then(json => {
+        var tranlatedtext = json.contents.translated;
+        outputtextarea.innerText=tranlatedtext;
+    })
+})
